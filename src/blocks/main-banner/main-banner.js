b407ae501577a817bfbceb09a15b8612ec4f303a
Lopachenok Sofya <Lopachenok.S@digdes.com>
2016-06-28 17:02:11 +0300

    add slider

---
+++ src/blocks/main-banner/main-banner.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#camera').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+});
